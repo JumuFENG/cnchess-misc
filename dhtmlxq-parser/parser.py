849d03feee3b10b234dc402fbbb7463ea9b584ea
chuazhou <chuazhou@cisco.com>
2021-01-05 13:28:51 +0800

    parse front or end of the chess in same vertical line.

--- dhtmlxq-parser/parser.py
+++ dhtmlxq-parser/parser.py
@@ -23,7 +23,7 @@ class ChessItem():
         self.pos = ChessPos() if pos is None else pos
         
     def move_to(self, pos):
-        self.pos = pos
+        self.pos = ChessPos() if pos is None else pos
 
 class Parser():
     def __init__(self):
@@ -32,10 +32,10 @@ class Parser():
             ChessItem('rK','帅'), ChessItem('rA2','仕'), ChessItem('rE2','相'), ChessItem('rH2','马'),
             ChessItem('rR2','车'), ChessItem('rC1','炮'), ChessItem('rC2','炮'), ChessItem('rP1','兵'), 
             ChessItem('rP2','兵'), ChessItem('rP3','兵'), ChessItem('rP4','兵'), ChessItem('rP5','兵'),
-            ChessItem('gR1','车'), ChessItem('gH1','马'), ChessItem('gE1','象'), ChessItem('gA1','士'), 
-            ChessItem('gK','将'), ChessItem('gA2','士'), ChessItem('gE2','象'), ChessItem('gH2','马'),
-            ChessItem('gR2','车'), ChessItem('gC1','炮'), ChessItem('gC2','炮'), ChessItem('gP1','卒'), 
-            ChessItem('gP2','卒'), ChessItem('gP3','卒'), ChessItem('gP4','卒'), ChessItem('gP5','卒'),
+            ChessItem('bR1','车'), ChessItem('bH1','马'), ChessItem('bE1','象'), ChessItem('bA1','士'), 
+            ChessItem('bK','将'), ChessItem('bA2','士'), ChessItem('bE2','象'), ChessItem('bH2','马'),
+            ChessItem('bR2','车'), ChessItem('bC1','炮'), ChessItem('bC2','炮'), ChessItem('bP1','卒'), 
+            ChessItem('bP2','卒'), ChessItem('bP3','卒'), ChessItem('bP4','卒'), ChessItem('bP5','卒'),
         ]
 
         self.line_labels = ['九', '八', '七', '六', '五', '四', '三', '二', '一']
@@ -44,7 +44,7 @@ class Parser():
         if not isinstance(binit, str) or not len(binit) == 64:
             binit = '8979695949392919097717866646260600102030405060708012720323436383'
 
-        iinit = [ChessPos(x, y) for x,y in zip([int(i) for i in binit[::2]], [int(j) for j in binit[1::2]])]
+        iinit = [ChessPos(xy = binit[i: i+2]) for i in range(0, len(binit), 2)]
 
         if not len(iinit) == len(self.all_chess):
             print("input error!")
@@ -57,6 +57,13 @@ class Parser():
         for c in self.all_chess:
             if c.pos.x == pos.x and c.pos.y == pos.y:
                 return c
+
+    def get_same_chess_vline(self, c):
+        cs = []
+        for x in self.all_chess:
+            if x.id[0] == c.id[0] and x.name == c.name and x.pos.x == c.pos.x and not x.pos.y == c.pos.y:
+                cs.append(x)
+        return cs
 
     def get_line_label(self, cid, pos):
         if cid[0] == 'r':
@@ -71,6 +78,47 @@ class Parser():
             return '进' if y1 > y2 else '退'
         return '进' if y1 < y2 else '退'
 
+    def get_front_end_str(self, c, others):
+        if not isinstance(others, list):
+            print('make sure the others are a list')
+            return
+
+        fe = ''
+        y = c.pos.y
+        if len(others) == 1:
+            o = others[0]
+            if c.id[0] == 'r':
+                fe = '后' if y > o.pos.y else '前'
+            else:
+                fe = '后' if y < o.pos.y else '前'
+            if c.id[1] == 'P':
+                fe += c.name + self.get_line_label(c.id, c.pos)
+            else:
+                fe += c.name
+            return fe
+        if len(others) == 2:
+            y1 = others[0].pos.y
+            y2 = others[1].pos.y
+            if y > y1 and y > y2:
+                fe = '后' if c.id[0] == 'r' else '前'
+            elif y < y1 and y < y2:
+                fe = '前' if c.id[0] == 'r' else '后'
+            else:
+                fe = '中'
+            return fe + c.name + self.get_line_label(c.id, c.pos)
+        else:
+            others.sort(key = lambda o: o.pos.y)
+            max_y = others[-1].pos.y
+            min_y = others[0].pos.y
+            if y > max_y:
+                fe = '后' if c.id[0] == 'r' else '前'
+            elif y < min_y:
+                fe = '前' if c.id[0] == 'r' else '后'
+            else:
+                lty = sum(1 for o in others if o.pos.y < y)
+                fe = self.line_labels[-lty-1] if c.id[0] == 'r' else str(len(others) - lty + 1)
+            return fe + c.name + self.get_line_label(c.id, c.pos)
+
     def get_distance_str(self, cid, y1, y2):
         if cid[0] == 'r':
             return self.line_labels[-(y1 - y2)]
@@ -82,8 +130,12 @@ class Parser():
         c = self.get_chess_by_pos(pos1)
         if c is None:
             print('no chess located in pos: ', pos1.x, pos1.y)
-        
-        stp = c.name + self.get_line_label(c.id, pos1)
+        co = self.get_same_chess_vline(c)
+        stp = ''
+        if len(co) > 0:
+            stp = self.get_front_end_str(c, co)
+        else:
+            stp = c.name + self.get_line_label(c.id, pos1)
         if pos1.y == pos2.y:
             stp += '平' + self.get_line_label(c.id, pos2)
         else:
@@ -93,6 +145,9 @@ class Parser():
             else:
                 stp += self.get_line_label(c.id, pos2)
 
+        d = self.get_chess_by_pos(pos2)
+        if d is not None:
+            d.move_to(None)
         c.move_to(pos2)
         return stp
 
