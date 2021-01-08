f77f6d40e01d06fdb3c0d30743364de0b0c8606f
chuazhou <chuazhou@cisco.com>
2021-01-08 17:29:30 +0800

    use ChessStep to remember the comments.

--- xqparser/dhtmlxq.py
+++ xqparser/dhtmlxq.py
@@ -7,6 +7,9 @@ class ChessPos():
     def __init__(self, x = -1, y = -1, xy = None):
         self.set_pos(x, y, xy)
 
+    def __str__(self):
+        return '(%s, %s)' % (self.x, self.y)
+
     def set_pos(self, x = None, y = None, xy = None):
         if xy is None:
             self.x = int(x)
@@ -24,8 +27,25 @@ class ChessItem():
         self.name = name
         self.pos = ChessPos() if pos is None else pos
         
+    def __str__(self):
+        return self.name + str(self.pos)
+
     def move_to(self, pos):
         self.pos = ChessPos() if pos is None else pos        
+
+class ChessStep():
+    def __init__(self, step, comment = None, desc = None):
+        self.step = step
+        self.comment = comment
+        self.desc = desc
+        self.src = ChessPos(xy = step[0:2])
+        self.dest = ChessPos(xy = step[2:4])
+
+    def __str__(self):
+        return self.step if self.desc is None else self.desc
+
+    def full_step(self):
+        return self.__str__() if self.comment is None else self.__str__() + '  ' + self.comment
 
 class Parser():
     def __init__(self, show_comments = True):
@@ -42,8 +62,8 @@ class Parser():
         ]
 
         self.line_labels = ['九', '八', '七', '六', '五', '四', '三', '二', '一']
+        self.chess_game = {'move_list':[], 'comments':[]}
         self.step_lists = []
-        self.comments_list = []
         self.step_no = 0
 
     def init(self, binit = None):
@@ -132,13 +152,13 @@ class Parser():
             return self.line_labels[-abs(y1 - y2)]
         return str(abs(y1 - y2))
 
-    def translate_step(self, step, id):
-        self.step_no += 1
-        pos1 = ChessPos(xy = step[0:2])
-        pos2 = ChessPos(xy = step[2:4])
+    def translate_step(self, step):
+        pos1 = step.src
+        pos2 = step.dest
         c = self.get_chess_by_pos(pos1)
         if c is None:
             print('no chess located in pos: ', pos1.x, pos1.y)
+            return
         co = self.get_same_chess_vline(c)
         stp = ''
         if len(co) > 0:
@@ -154,56 +174,78 @@ class Parser():
             else:
                 stp += self.get_line_label(c.id, pos2)
 
+        rstp = step
+        rstp.desc = stp
         d = self.get_chess_by_pos(pos2)
         if d is not None:
             d.move_to(None)
         c.move_to(pos2)
-        if self.show_comments:
-            cmt = self.get_comment(self.step_no, id)
-            if cmt is not None:
-                return stp + '  ' + cmt['comment']
-        return stp
+        return rstp
 
-    def translate_steps(self, steps, id = None):
-        if id is None or int(id) == 0:
-            self.step_lists.append({'id': 0, 'steps': steps, 'baseid': 0, 'start_node':0})
-        stps = [self.translate_step(steps[i:i+4], int(id)) for i in range(0,len(steps),4)]
+    def translate_steps(self, steps):
+        self.init()
+        stps = [self.translate_step(s) for s in self.get_whole_steps(steps['id'])]
         for s in stps:
-            print(s)
+            print(s.full_step() if self.show_comments else s)
             
     def get_whole_steps(self, id):
         basestep = [s for s in self.step_lists if s['id'] == int(id)][0]
         if int(id) == 0 or basestep['id'] == basestep['baseid']:
             return basestep['steps']
         basesteps = self.get_whole_steps(basestep['baseid'])
-        return basesteps[0:4*basestep['start_node'] - 4] + basestep['steps']
+        return basesteps[0:basestep['start_node'] - 1] + basestep['steps']
 
-    def translate_sub_steps(self, steps, baseid, node, subid):
-        self.step_lists.append({'id':int(subid), 'steps':steps, 'baseid': int(baseid), 'start_node':int(node)})
-        self.init()
-        self.translate_steps(self.get_whole_steps(subid), int(subid))
-
-    def get_comment(self, step, id = None):
-        id = 0 if id is None else id
-        for c in self.comments_list:
-            if c['step'] == step and c['step_list_id'] == id:
-                return c
+    def match_step_comment(self, mvlist, comments):
+        mvlist.sort(key = lambda m: m['id'])
+        step_lists = []
+        for m in mvlist:
+            step = {}
+            step['id'] = m['id']
+            step['baseid'] = m['baseid']
+            step['start_node'] = m['start_node']
+            step['steps'] = [ChessStep(m['steps'][i:i+4]) for i in range(0,len(m['steps']),4)]
+            for i in range(0, len(step['steps'])):
+                comment = None
+                for c in comments:
+                    if c['step_list_id'] == m['id'] and c['step'] == i + 1:
+                        comment = c['comment']
+                        break
+                step['steps'][i].comment = comment
+            step_lists.append(step)
+        return step_lists
 
     def load_comments(self, cmtstr):
         cmts = re.findall(r'\[DhtmlXQ_comment([\d]+)\](.*?)\[/DhtmlXQ_comment([\d]+)\]', cmtstr, re.DOTALL)
-        [self.comments_list.append({'step_list_id':0, 'step':int(c[0]), 'comment': c[1]}) for c in cmts if c[0] == c[2]]
+        [self.chess_game['comments'].append({'step_list_id':0, 'step':int(c[0]), 'comment': c[1]}) for c in cmts if c[0] == c[2]]
         cmts = re.findall(r'\[DhtmlXQ_comment([\d]+)_([\d]+)\](.*?)\[/DhtmlXQ_comment([\d]+)_([\d]+)\]', cmtstr, re.DOTALL)
-        [self.comments_list.append({'step_list_id':int(c[0]), 'step':int(c[1]), 'comment': c[2]}) for c in cmts if c[0] == c[3] and c[1] == c[4]]
+        [self.chess_game['comments'].append({'step_list_id':int(c[0]), 'step':int(c[1]), 'comment': c[2]}) for c in cmts if c[0] == c[3] and c[1] == c[4]]
 
     def load_dhtml(self, text):
         self.load_comments(text)
         title = re.search(r'\[DhtmlXQ_title\](.*?)\[/DhtmlXQ_title\]', text).group(1).strip()
+        self.chess_game['title'] = title
         binit = re.search(r'\[DhtmlXQ_binit\](.*?)\[/DhtmlXQ_binit\]', text).group(1).strip()
-        self.init(binit)
+        self.chess_game['binit'] = None if binit == '8979695949392919097717866646260600102030405060708012720323436383' else binit
         mvlist = re.search(r'\[DhtmlXQ_movelist\](.*?)\[/DhtmlXQ_movelist\]', text).group(1).strip()
-        self.translate_steps(mvlist, 0)
+        self.chess_game['move_list'].append({'id': 0, 'steps': mvlist, 'baseid': 0, 'start_node':0})
         sublist = re.findall(r'\[DhtmlXQ_move_([\d]+)_([\d]+)_([\d]+)\](.*?)\[/DhtmlXQ_move_([\d]+)_([\d]+)_([\d]+)\]', text, re.DOTALL)
-        [self.translate_sub_steps(s[3], s[0], s[1], s[2]) for s in sublist if s[0] == s[4] and s[1] == s[5] and s[2] == s[6]]
+        [self.chess_game['move_list'].append({
+            'id':int(s[2]), 'steps':s[3], 'baseid': int(s[0]), 'start_node':int(s[1])
+            }) for s in sublist if s[0] == s[4] and s[1] == s[5] and s[2] == s[6]]
+        print(self.chess_game)
+        self.step_lists = self.match_step_comment(self.chess_game['move_list'], self.chess_game['comments'])
+        [self.translate_steps(s) for s in self.step_lists]
 
 if __name__ == '__main__':
-    p = Parser()
+    pos = ChessPos(xy ='12')
+    print(pos)
+    chs = ChessItem('rR1','车')
+    print(chs)
+    chs.move_to(pos)
+    print(chs)
+    step = ChessStep('1122')
+    print(step)
+    step.desc = 'j1j1'
+    print(step)
+    step.comment = 'comment'
+    print(step.full_step())
